9619cf96612877ec2a177e4cfec6f9e902c942ff
DixonKH <khaydarovdilshod9@gmail.com>
2024-02-24 19:17:51 +0900

    feat: integration of environmental variables

--- train.js
+++ train.js
@@ -1,19 +1,19 @@
 //  ===========================  Task-G
 
-function getHighestIndex(arr) {
-    if(Array.isArray(arr)) {
-       let largest = arr[0];
-       for(let i = 0; i < arr.length; i++) {
-          if(arr[i] > largest) {
-             largest = arr[i];
-            }
-          largeIndex = arr.indexOf(largest); 
-          }
-       }
-       return largeIndex;
+// function getHighestIndex(arr) {
+//     if(Array.isArray(arr)) {
+//        let largest = arr[0];
+//        for(let i = 0; i < arr.length; i++) {
+//           if(arr[i] > largest) {
+//              largest = arr[i];
+//             }
+//           largeIndex = arr.indexOf(largest); 
+//           }
+//        }
+//        return largeIndex;
           
-       }
+//        }
  
- const result = getHighestIndex([4, 10, 26, 25, 24, 26]);
+//  const result = getHighestIndex([4, 10, 26, 25, 24, 26]);
  
- console.log("heighestIndex: ", result);
+//  console.log("heighestIndex: ", result);
